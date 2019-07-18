37dbf10d66b643a5f99895c23877e321dc4d3f71
storres8 <stevetorres7562@gmail.com>
2019-07-17 22:06:17 -0400

    imported the reactice forms module from angular forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  projectForm: FormGroup;
 }
