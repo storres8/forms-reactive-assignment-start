47a3f8e979504d7620b8281934a68676be1fd37f
storres8 <stevetorres7562@gmail.com>
2019-07-17 23:27:49 -0400

    done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Observable } from "rxjs/observable";
 
 @Component({
   selector: "app-root",
@@ -12,21 +13,41 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      projectName: new FormControl(null, [
-        Validators.required,
-        this.forbiddenProjNames.bind(this)
-      ]),
+      projectName: new FormControl(
+        null,
+        [Validators.required],
+        [this.asyncFobiddenProjNames.bind(this)]
+      ),
       email: new FormControl(null, [Validators.required, Validators.email]),
       status: new FormControl()
     });
+
+    this.projectForm.statusChanges.subscribe(status => {
+      console.log(`Form is ${status}`);
+    });
   }
 
-  forbiddenProjNames(control: FormControl): { [s: string]: boolean } {
-    if (this.invalidProjNames.indexOf(control.value) !== -1) {
-      return { forbiddenProjectName: true };
-    } else {
-      return null;
-    }
+  // This is the same custom Validator as bellow except this one is sybchronus.
+
+  // forbiddenProjNames(control: FormControl): { [s: string]: boolean } {
+  //   if (this.invalidProjNames.indexOf(control.value) !== -1) {
+  //     return { forbiddenProjectName: true };
+  //   } else {
+  //     return null;
+  //   }
+  // }
+
+  asyncFobiddenProjNames(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((res, rej) => {
+      setTimeout(() => {
+        if (this.invalidProjNames.indexOf(control.value) !== -1) {
+          res({ forbiddenProjectName: true });
+        } else {
+          res(null);
+        }
+      }, 1000);
+    });
+    return promise;
   }
 
   onSubmit() {
