7cc2c69d43c34d61b0793fd74a1d9c2658e3c4f7
storres8 <stevetorres7562@gmail.com>
2019-07-17 22:20:14 -0400

    created project name label and input form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from "@angular/core";
-import { FormGroup } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(null),
+      email: new FormControl(null),
+      status: new FormControl()
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
 }
