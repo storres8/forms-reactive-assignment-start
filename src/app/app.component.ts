296105a056bdae02a728992e05ed2be44b1f1c97
storres8 <stevetorres7562@gmail.com>
2019-07-17 23:05:40 -0400

    added validations and css to show if project name is invalid on the form,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,25 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 })
 export class AppComponent implements OnInit {
   projectForm: FormGroup;
+  invalidProjNames = ["Test"];
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      projectName: new FormControl(null, [Validators.required]),
+      projectName: new FormControl(null, [
+        Validators.required,
+        this.forbiddenProjNames.bind(this)
+      ]),
       email: new FormControl(null, [Validators.required, Validators.email]),
       status: new FormControl()
     });
+  }
+
+  forbiddenProjNames(control: FormControl): { [s: string]: boolean } {
+    if (this.invalidProjNames.indexOf(control.value) !== -1) {
+      return { forbiddenProjectName: true };
+    } else {
+      return null;
+    }
   }
 
   onSubmit() {
